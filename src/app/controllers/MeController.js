3e82abb48c9c57c42d255deb54ae4ac6c278771d
Nguyễn Văn Phát <82584665+phat-612@users.noreply.github.com>
2024-03-13 23:16:04 +0700

    a

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -6,5 +6,8 @@ class MeController {
   history(req, res, next) {
     res.render("user/profiles/history", { layout: "userProfile" });
   }
+  address(req, res, next) {
+    res.render("user/profiles/address", { layout: "userProfile" });
+  }
 }
 module.exports = new MeController();
--- src/routes/me.js
+++ src/routes/me.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const meController = require("../app/controllers/MeController");
 
+router.get("/address", meController.address);
 router.get("/history", meController.history);
 router.get("/", meController.profile);
 
